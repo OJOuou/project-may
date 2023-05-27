5d7b605daae716c1592659d4aed25df2c8f6734f
jeje <jeed02@naver.com>
2023-05-27 21:44:52 +0900

    init

---
+++ src/main/java/com/project/projectmay/ProjectMayApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectmay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/projectmay/ProjectMayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projectmay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
